830946abba0ff1dc000395448c876e02643c6f5f
paudelcaesar27 <paudelcaesar27@gmail.com>
2020-09-19 22:21:56 -0500

    setup for services

---
+++ eurekaDiscoveryServer/src/main/java/com/edu/cs455/eurekadiscoveryserver/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.edu.cs455.eurekadiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
